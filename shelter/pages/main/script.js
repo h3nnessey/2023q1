23123bd5e93afd55507cb7fbf807f9112d7bb266
Vitaliy <vitaliy.zayakin@gmail.com>
2023-04-10 15:54:43 +0500

    feat: add slider adaptive for 2/1 cards at screen scenario

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -5,9 +5,10 @@ const slider = document.querySelector('.pets-slider__track');
 let cardsContainer = document.querySelector('.pets-cards');
 const nextBtn = document.querySelector('.pets-slider-btn__right');
 const prevBtn = document.querySelector('.pets-slider-btn__left');
-const btns = document.querySelectorAll('.pets-slider__btn');
+const btns = [nextBtn, prevBtn];
+const root = document.querySelector(':root');
 
-const idPool = pets.map(pet => pet.id);
+const idPool = shuffle(pets.map(pet => pet.id));
 
 let state = {
   previous: [],
@@ -15,23 +16,52 @@ let state = {
   next: [],
 };
 
-const initRandomIds = () => {
-  state.previous = idPool.slice(0, 3);
-  state.current = shuffle(idPool.filter(id => !state.previous.includes(id))).slice(0, 3);
-  state.next = shuffle(idPool.filter(id => !state.current.includes(id))).slice(0, 3);
-  console.log(state.previous, state.current, state.next, 'initial');
+const getCardsCount = () => {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 1279) {
+    root.style.setProperty('--slider-left-initial', '-109%');
+    root.style.setProperty('--slider-left-end', '-218%');
+    return 3;
+  }
+  if (windowWidth >= 768 && windowWidth <= 1279) {
+    root.style.setProperty('--slider-left-initial', '-107%');
+    root.style.setProperty('--slider-left-end', '-214%');
+    return 2;
+  }
+  if (windowWidth <= 767) {
+    root.style.setProperty('--slider-left-initial', '-102%');
+    root.style.setProperty('--slider-left-end', '-206%');
+    return 1;
+  }
+};
+
+let cardsCount = getCardsCount();
+
+const initState = () => {
+  state.previous = shuffle(idPool.slice(0, cardsCount));
+  state.current = getRandomIdsWhichAreNotInCollection(state.previous);
+  state.next = getRandomIdsWhichAreNotInCollection(state.current);
 };
 
 const initSlider = () => {
+  initState();
+  renderCards(cardsContainer);
+  cardsContainer.addEventListener('animationend', rerenderCards);
+};
+
+const rerenderSlider = () => {
+  initState();
+  cardsContainer.innerHTML = '';
+  renderCards(cardsContainer);
+};
+
+const renderCards = container => {
   for (const array in state) {
     state[array].forEach(id => {
       const pet = pets.find(pet => pet.id === id);
-      cardsContainer.insertAdjacentElement('beforeend', createCardTemplate(pet));
+      container.insertAdjacentElement('beforeend', createCardTemplate(pet));
     });
   }
-  cardsContainer.addEventListener('animationend', () => {
-    rerenderCards();
-  });
 };
 
 const createCardTemplate = obj => {
@@ -52,36 +82,33 @@ const createCardTemplate = obj => {
 const rerenderCards = () => {
   const newCardsContainer = document.createElement('div');
   newCardsContainer.classList.add('pets__cards', 'pets-cards');
-  for (const array in state) {
-    state[array].forEach(id => {
-      const pet = pets.find(pet => pet.id === id);
-      newCardsContainer.insertAdjacentElement('beforeend', createCardTemplate(pet));
-    });
-  }
+  renderCards(newCardsContainer);
   cardsContainer.replaceWith(newCardsContainer);
   btns.forEach(btn => (btn.disabled = false));
 };
 
 const addSliderAnimation = direction => {
-  cardsContainer.style.animation = `0.4s ease-in-out 0s 1 forwards slide-to-${direction}`;
+  cardsContainer.style.animation = `0.4s ease 0s 1 forwards slide-to-${direction}`;
+};
+
+const getRandomIdsWhichAreNotInCollection = collection => {
+  return shuffle(idPool.filter(id => !collection.includes(id))).slice(0, cardsCount);
 };
 
 const handleNextClick = () => {
   state.previous = state.current;
   state.current = state.next;
-  state.next = shuffle(idPool.filter(id => !state.current.includes(id))).slice(0, 3);
-  console.log(state.previous, state.current, state.next, 'next');
-  addSliderAnimation('right');
+  state.next = getRandomIdsWhichAreNotInCollection(state.current);
   nextBtn.disabled = true;
+  addSliderAnimation('right');
 };
 
 const handlePrevClick = () => {
   state.next = state.current;
   state.current = state.previous;
-  state.previous = shuffle(idPool.filter(id => !state.current.includes(id))).slice(0, 3);
-  console.log(state.previous, state.current, state.next, 'prev');
-  addSliderAnimation('left');
+  state.previous = getRandomIdsWhichAreNotInCollection(state.current);
   prevBtn.disabled = true;
+  addSliderAnimation('left');
 };
 
 const mutationCallback = (mutationsList, observer) => {
@@ -93,16 +120,26 @@ const mutationCallback = (mutationsList, observer) => {
     }
   });
 };
+
 const mutationObserver = new MutationObserver(mutationCallback);
 
 mutationObserver.observe(slider, {
   childList: true,
 });
 
-initRandomIds();
 initSlider();
 
 nextBtn.addEventListener('click', handleNextClick);
 prevBtn.addEventListener('click', handlePrevClick);
 
+window.addEventListener('resize', () => {
+  const howMuchCardsShouldBe = getCardsCount();
+  if (cardsCount !== howMuchCardsShouldBe) {
+    cardsCount = howMuchCardsShouldBe;
+    rerenderSlider();
+  }
+});
+
 // todo: no repeat cards in prev and next, add min-width/min-height to pet image for prevent blinking, fix shadows (its hidden at Y-axis rn), refactor a whole file
+// write fetch for get pets
+// вынести в отдельный файл/файлы логику функции типа крейтов карточки и рандомайзера
