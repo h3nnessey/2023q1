4085a2388846c8dfa50e716a98ecd3145d421eb9
Vitaliy <vitaliy.zayakin@gmail.com>
2023-07-05 00:51:19 +0500

    feat: update styles for no-scroll tech req

--- rss-css-selectors/src/components/header/Header.ts
+++ rss-css-selectors/src/components/header/Header.ts
@@ -3,6 +3,6 @@ import { BaseComponent } from '../baseComponent/BaseComponent';
 
 export class Header extends BaseComponent {
   constructor() {
-    super({ tagName: 'header', classNames: ['header'], html: '<span>Cards Selector Game</span>' });
+    super({ tagName: 'header', classNames: ['header'], html: '<h1 class="header__title">Cards Selector Game</h1>' });
   }
 }
--- rss-css-selectors/src/constants/index.ts
+++ rss-css-selectors/src/constants/index.ts
@@ -1,3 +1,3 @@
 export const CSS_CLASSES_TO_EXCLUDE: string[] = ['active', 'pl-20', 'ct', 'hovered', 'target'];
 export const LESSON_TARGET_CLASS = 'target';
-export const LINE_NUMBERS_COUNT = 20;
+export const LINE_NUMBERS_COUNT = 15;
