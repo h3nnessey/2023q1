c855a34a9b1a476d53ab96e8082b1729a1084bf7
Vitaliy <vitaliy.zayakin@gmail.com>
2023-07-25 15:45:05 +0500

    fix: add missing minimizer for js in webpack config

--- async-race/webpack.config.js
+++ async-race/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 const mode = process.env.NODE_ENV;
 
@@ -56,7 +57,7 @@ module.exports = {
     }),
   ],
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimizer: [new CssMinimizerPlugin(), new TerserWebpackPlugin()],
     minimize: mode === 'production',
   },
 };
